75fbef5650216833bdef066931efa82f31349764
Geoffrey Kong <79559478+CYBRXT@users.noreply.github.com>
2022-07-01 01:19:39 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  username: process.env.TEXTNOW_USERNAME || '',
-  password: process.env.TEXTNOW_PASSWORD || '',
+  username: process.env.TEXTNOW_USERNAME || 'sideloadingpiracy@gmail.com',
+  password: process.env.TEXTNOW_PASSWORD || 'Iamkeith1234!',
   cookies: process.env.TEXTNOW_COOKIES || '',
   recipient: process.env.TEXTNOW_RECIPIENT || '(726) 666-0002',
   message: process.env.TEXTNOW_MESSAGE || 'auto-send message',
